86bbe30da0d9c31f0161094227335305073dfa1a
sumitkm <sumitkm@gmail.com>
2013-08-06 13:59:51 +0530

    Code Commit

---
+++ HelloAngularJs/Scripts/hello-angular.js
@@ -0,0 +1,76 @@
+﻿/// <reference path="jquery-2.0.3.js" />
+/// <reference path="hello-angular.js" />
+/// <reference path="angular.js" />
+
+// Create ngTwitter Module (roughly Module = namespace in C#)
+var ngTwitter = angular.module("ngTwitter", ['ngResource']);
+
+// Declaring a Service
+ngTwitter.factory("TwitterService", function ($resource)
+{
+    return {
+        timeline: $resource("/Home/Tweet")
+    }
+});
+
+ngTwitter.directive("retweetButton", function ()
+{
+    return {
+        restrict: "E",
+        replace: true,
+        scope: {
+            text: "@",
+            clickevent: "&"
+        },
+        template: "<button class='btn btn-mini' ng-click='clickevent()'><i class='icon-retweet'></i> {{text}}</button>"
+    };
+});
+
+ngTwitter.controller("TimelineController", function ($scope, $http, TwitterService)
+{
+    $scope.tweets = TwitterService.timeline.query({}, isArray = true);
+
+    $scope.newTweets = {
+        0: "No new Tweets",
+        other: "{} new Tweets"
+    };
+
+    $scope.sendStatus = function ()
+    {
+        var tweetText = $scope.statusText;
+        var newTimeLine = new TwitterService.timeline(
+            {
+                Tweet: tweetText
+            });
+        newTimeLine.$save(function (data, headers)
+        {
+            if (data.success && data.success == true)
+            {
+                alert("Tweet Sent Successfully!");
+                $scope.statusText = "";
+            }
+            else
+            {
+                alert("ERROR: " + data.errorMessage);
+            }
+        });
+    };
+
+    $scope.retweet = function (item)
+    {
+        var resultPromise = $http.post("/Home/Retweet/", item);
+        resultPromise.success(function (data)
+        {
+            if (data.success)
+            {
+                alert("Retweeted successfully");
+            }
+            else
+            {
+                alert("ERROR: Retweeted failed! " + data.errorMessage);
+            }
+        });
+    };
+
+
+});
--- README.md
+++ README.md
@@ -1,4 +1,12 @@
-angularjs-directives
+AngularJS - Directives
 ====================
 
-Next post in the AngularJS series showing how to use Create Directives in Angular
+The next post in the AngularJS series showing how to use Create Directives in Angular JS in an ASP.NET MVC Twitter client application.
+
+Previous articles in the series
+-----------------
+Part 1 - [Hello AngularJS – Building a Tweet Reader](http://www.devcurry.com/2013/06/hello-angularjs-building-tweet-reader.html)
+
+Part 2 - [Using AngularJS Modules and Services Feature in an ASP.NET MVC application](http://www.devcurry.com/2013/06/using-angularjs-modules-and-services.html)
+
+Part 3 - [AngularJS – Post data using the $resource Service in an ASP.NET MVC app](http://www.devcurry.com/2013/07/angularjs-post-data-using-resource.html)
